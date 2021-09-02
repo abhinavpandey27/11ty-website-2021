f544f8b2765f08d73c24b69288b0a223e5ab8582
Abhinav Pandey <adhinavpandey@Adhinavs-MacBook-Pro.local>
2021-09-03 00:11:18 +0530

    initial commit

---
+++ gulp-tasks/fonts.js
@@ -0,0 +1,20 @@
+const {dest, src} = require('gulp');
+const GetGoogleFonts = require('get-google-fonts');
+
+const fonts = async () => {
+  // Setup of the library instance by setting where we want
+  // the output to go. CSS is relative to output font directory
+  const instance = new GetGoogleFonts({
+    outputDir: './dist/fonts',
+    cssFile: './fonts.css'
+  });
+
+  // Grabs fonts and CSS from google and puts in the dist folder
+  const result = await instance.download(
+    'https://fonts.googleapis.com/css2?family=Azeret+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Besley:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'
+  );
+
+  return result;
+};
+
+module.exports = fonts;
